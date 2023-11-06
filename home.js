40c3f73a40bbaabcb8f88e4adcd678c4682fdaf9
Mohammad Al-Shalabi <91227088+Malshalab@users.noreply.github.com>
2023-11-05 21:03:48 -0500

    Add files via upload

---
+++ home.js
@@ -0,0 +1,10 @@
+function Show(){
+    var x = document.getElementById('contact-form') ;
+    x.style.display='flex';
+}
+
+function Hide(){
+    var x =document.getElementById('contact-form');
+    x.style.display='none';
+}
+
